98a7fafe561b313fa22280e8f699d4edd689f5ef
masanari9256 <masanari92562@gmail.com>
2022-01-03 04:31:44 +0900

    [update]
    ヒーローを取得&更新したときにメッセージを表示するようにした

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Hero} from "../hero";
 import {HEROES} from "../mock-heroes";
 import {HeroService} from "../hero.service";
+import {MessageService} from "../message.service";
 
 @Component({
   selector: 'app-heroes',
@@ -13,17 +14,21 @@ export class HeroesComponent implements OnInit {
 
   selectedHero?: Hero
 
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {
+    this.getHeroes()
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes)
-  }
-
-  constructor(private heroService: HeroService) {
-    this.getHeroes()
   }
 
   ngOnInit(): void {
